95ca040f53c1abb8456c925d65e7c3cad4e7f20d
warmperfume <warmperfume@live.cn>
2015-10-18 04:08:18 -0400

    Add mySpectralFlux.m

---
+++ HW2/mySpectralFlux.m
@@ -0,0 +1,26 @@
+function [ spectralFlux ] = mySpectralFlux( x, windowSize, hopSize )
+% computes the spectral flux for onset detection
+% number of results
+blockNum        = ceil(length(x)/hopSize);
+
+% zero-padding the signal
+xAllZeros               = zeros(1,blockNum * hopSize + windowSize - 1);
+xAllZeros(1:length(x))  = x;
+paddedSignal            = xAllZeros;
+
+% allocate memory
+spectrumsOfDividedSignal    = zeros(blockNum,windowSize);
+spectralFlux                = zeros(1,blockNum-1);
+
+% divide the signal into blocks
+for n = 1:blockNum
+	i_start         = (n-1) * hopSize + 1;
+	i_stop          = i_start + windowSize - 1;
+    spectrumsOfDividedSignal(n, :)   = fft(paddedSignal(i_start:i_stop)); 
+end
+    difference          = diff(spectrumsOfDividedSignal);
+    conjuOfDifference   = conj(difference);
+    spectralFlux        = sqrt(sum(difference.*conjuOfDifference))/windowSize;
+
+end
+
--- HW2/signalGenerator.m
+++ HW2/signalGenerator.m
@@ -1,4 +1,5 @@
 function [ signal ] = signalGenerator( samplingRate, startTimeInSeconds, endTimeInSeconds, frequencyInHerz, amplitude )
+% Generate signal by using samplingRate, startTimeInSeconds, endTimeInSeconds, frequencyInHerz and amplitude
 t = samplingRate * startTimeInSeconds:(samplingRate * endTimeInSeconds - 1);
 signal = amplitude * sin(frequencyInHerz * 2* pi * t / samplingRate);
 end
